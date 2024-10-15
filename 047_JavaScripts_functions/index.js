5a052c5631d477120366657dfa3fddcfaf1ff4a3
Aman Sherjada Khan <81584983+amansherjada@users.noreply.github.com>
2024-10-15 19:05:14 +0530

    Add files via upload

---
+++ 047_JavaScripts_functions/index.js
@@ -0,0 +1,15 @@
+// console.log("Heyyyyy")
+// console.log("Heyyyyy Hello")
+// console.log("Heyyyyy How")
+// console.log("Heyyyyy are")
+// console.log("Heyyyyy you")
+
+function nice(name) {
+    console.log("Heyyyyy " + name)
+    console.log("Heyyyyy "+ name + " Hello")
+    console.log("Heyyyyy " + name + " How")
+    console.log("Heyyyyy " + name + " are")
+    console.log("Heyyyyy " + name + " you")
+}
+
+nice("Aman")
