5a64a83c068969fbe7dd5dd4b7915c595e218796
Aman Sherjada Khan <81584983+amansherjada@users.noreply.github.com>
2024-10-16 13:39:48 +0530

    Add files via upload

---
+++ 053_JavaScript_selecting_by_id_classes_etc/script.js
@@ -0,0 +1,19 @@
+console.log("printing......")
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[4].style.backgroundColor = "aqua"
+
+// document.getElementById("redbox").style.backgroundColor = "red"; //box with id "redbox" will get red bg color
+
+//document.querySelector(".box").style.backgroundColor = "green"; //first box green
+
+// document.querySelectorAll(".box").forEach(element => {
+//     // console.log(element)
+//     element.style.backgroundColor = "green";
+// });
+
+console.log(document.getElementsByTagName("div"))
+
+// Refer JS notes Chapter 7 for more
